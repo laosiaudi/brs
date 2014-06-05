75947e3efa57ea519cddadf51962ffd4e8d878a8
JinnieTsai <caijin11331007@gmail.com>
2014-06-05 22:56:40 +0800

    search.py updated

--- search.py
+++ search.py
@@ -3,11 +3,17 @@
 # FILE:     search.py
 # 2014 @laosiaudi All rights reserved
 # CREATED:  2014-06-05 20:04:49
-# MODIFIED: 2014-06-05 21:43:33
+# MODIFIED: 2014-06-05 22:49:15
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import MySQLdb
+import json
+
+db = MySQLdb.connect(host= "localhost", user= "JinnieTsai", passwd= "cj", db
+        = "bookdb")
+cur = db.cursor()
 
 class SearchHandler(tornado.web.RequestHandler):
     def post(self):
@@ -15,10 +21,16 @@ class SearchHandler(tornado.web.RequestHandler):
         category = self.get_argument("category")
 
         if category == 'ISBN':
-            pass
-        elif category == 'bookname':
-            pass
-        elif category == 'author':
-            pass
-        
+            cur.execute("SELECT * FROM book_info WHERE isbn = %s", keyword)
+            book = cur.fetchone()
+            self.write(json.dump(book))
 
+        elif category == 'bookname':
+            cur.execute("SELECT * FROM book_info WHERE book_name LIKE %%s%", keyword)
+            books = cur.fetchall()
+            self.write(json.dump(books))
+
+        elif category == 'author':
+            cur.execute("SELECT * FROM book_info WHERE author LIKE %%s%", keyword)
+            books = cur.fetchall()
+            self.write(json.dump(books))
