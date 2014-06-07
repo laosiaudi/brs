419e45b3ab7a85068e4d4955da434a51ab38a91c
yakiang <yakiang7@gmail.com>
2014-06-08 00:55:07 +0800

    fix settings bug

--- static/js/settings.js
+++ static/js/settings.js
@@ -2,5 +2,23 @@ $(document).ready(function() {
   for (var i in tags) {
     $($('.checkbox input:checkbox')[tags[i]]).prop('checked', true)
   }
+
+  $('#settingsform').submit(function(e) {
+    $('#status').text('');
+    $.ajax({
+      type: 'POST',
+      data: $('#settingsform').serialize(),
+      success: show_status
+    });
+    return false;
+  });
 });
+
+function show_status(status) {
+  if (status == '1') {
+    $('#status').text('修改成功！');
+  } else {
+    $('#status').text('修改失败！');
+  }
+}
 
