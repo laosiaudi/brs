b961ff47dad348ae76d22d306fd58a4900d7d978
yakiang <yakiang7@gmail.com>
2014-06-07 11:25:15 +0800

    add ajax

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+
+  $('#loginform').submit(function(e) {
+    $.ajax({
+      type: 'POST',
+      data: $('#loginform').serialize(),
+      success: show_status
+    });
+    return false;
+  });
 });
+
+function show_status() {
+  $('#status').text('okkkkay');
+}
