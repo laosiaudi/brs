ff4320ea5ed10619e2a62576a3f4352df2794b11
yakiang <yakiang7@gmail.com>
2014-06-07 22:54:29 +0800

    add search bar

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,19 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+
+  $('#btn-one').attr("disabled", true);
+  $('#btn-two').attr("disabled", false);
+  $('#by').val('title');
+  $('#btn-two').click(function() {
+    $('#btn-one').attr("disabled", false);
+    $('#btn-two').attr("disabled", true);
+    $('#by').val('author');
+  });
+  $('#btn-one').click(function() {
+    $('#btn-one').attr("disabled", true);
+    $('#btn-two').attr("disabled", false);
+    $('#by').val('title');
+  });
 });
 
